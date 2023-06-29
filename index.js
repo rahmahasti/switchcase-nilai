3a71faa472eb660c4a65c14c75f655596983d611
rahmahasti <117341286+rahmahasti@users.noreply.github.com>
2023-06-29 20:35:41 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const dicek = () =>{
+    const pilih = document.getElementById("nilai").value;
+
+    switch (pilih){
+        case "A":
+            alert("Nilai Anda 100")
+            break
+        case "B":
+            alert("Nilai Anda 80")
+            break
+        case "C":
+            alert("Nilai Anda 70")
+            break
+        case "D":
+            alert("Nilai Anda 60")
+            break
+
+    }
+}
